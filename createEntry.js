73bcdc2c86f43902c867bc4daf98d0f70c248cb2
y0092888 <ju-horn@web.de>
2020-07-15 16:08:43 +0200

    exported js code

---
+++ createEntry.js
@@ -0,0 +1,65 @@
+function appendEntry(todoObj) {
+  // *CREATE* to do item
+  const li = document.createElement("li");
+  li.classList.add("entry");
+
+  const input = document.createElement("input");
+  input.value = todoObj.content;
+  input.onkeyup = (e) => {
+    todoObj.content = input.value;
+    saveData();
+  };
+  li.appendChild(input);
+
+  // mark to-do item as *DONE*
+  const button_done = createButtonDone(todoObj, input);
+  li.appendChild(button_done);
+
+  // mark to-do item as *ARCHIVE*
+  const button_archive = createRemoveButton(todoObj, () =>
+    li.parentNode.removeChild(li)
+  );
+  li.appendChild(button_archive);
+
+  const ul_list = document.getElementsByClassName("entries")[0];
+  ul_list.appendChild(li);
+  // ul_list.reverse();
+}
+
+function createButtonDone(todoObj, input) {
+  const button_done = document.createElement("button");
+  button_done.innerHTML = "mark completed";
+  button_done.onclick = function (e) {
+    input.toggleAttribute("readonly");
+    todoObj.completed = !todoObj.completed;
+    if (todoObj.completed) {
+      button_done.innerHTML = "mark uncompleted";
+    } else {
+      button_done.innerHTML = "mark completed";
+    }
+    saveData();
+    // also: move item to done list?
+  };
+  if (todoObj.completed) {
+    // if supposed to be done
+    button_done.onclick();
+  }
+
+  return button_done;
+}
+
+function createRemoveButton(todoObj, removeYourself) {
+  const button_archive = document.createElement("button");
+  button_archive.innerHTML = "remove";
+  button_archive.onclick = function (e) {
+    const result = confirm("Do you really want to delete permanently?");
+    if (result) {
+      toDoListItems = toDoListItems.filter(
+        (element) => element.content !== todoObj.content
+      );
+      saveData();
+      removeYourself();
+    }
+  };
+  return button_archive;
+}
---
+++ loadData.js
@@ -0,0 +1,35 @@
+// loading data from localstorage to JS
+function loadData() {
+  let strToDoList = localStorage.getItem("todoList");
+  const toDoListItems = [];
+
+  if (strToDoList && strToDoList !== "") {
+    let items = strToDoList.split(";");
+    // "text;done;" --> "["text","done",""]"
+    // still to do: set item to "open" by default
+
+    for (let i = 0; i < items.length - 1; i += 2) {
+      const text = items[i];
+      const done = items[i + 1] === "done" ? true : false;
+      // array
+      toDoListItems.push(new ToDo(text, done));
+    }
+  }
+  return toDoListItems;
+}
+
+function ToDo(content, completed) {
+  this.content = content;
+  this.completed = completed;
+}
+
+function saveData() {
+  let data = "";
+  // [[text,done]]
+  for (let i = 0; i < toDoListItems.length; i++) {
+    const done = toDoListItems[i].completed ? "done" : "open";
+    data += toDoListItems[i].content + ";" + done + ";";
+  }
+  // string
+  localStorage.setItem("todoList", data);
+}
